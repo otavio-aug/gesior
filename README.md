15e334d4aba564955011eefc409c47ca6a68b4d1
httpsotavio <otavioaugustocardoso@gmail.com>
2024-07-02 00:56:43 -0300

    update headers

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-In order to use the landing page with an gesior AAC, follow commit `aaecfb9bea64b2e0da75ce19f02288e92109de4f`.
+In order to use the landing page with an gesior AAC, follow commit `aaecfb9bea64b2e0da75ce19f02288e92109de4f` (headers outdated, check last commit).
 
 Url:
 ```
 localhost/api/infos.php
-
--- api/infos.php
+++ api/infos.php
@@ -1,6 +1,9 @@
 <?php
     header('Content-Type: application/json');
 
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     require_once("../config/config.php");
 
     $servername = $config['database']['host'];
